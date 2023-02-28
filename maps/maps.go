b727b971f5c5003a3ef9399d4f5ed0ce7daaff34
ding.peng <ding.peng@email.cn>
2023-02-28 22:36:07 +0800

    .

--- maps/maps.go
+++ maps/maps.go
@@ -1,30 +1,12 @@
 package maps
 
 // 合并 map
-func Merge[K comparable, V any](maps ...map[K]V) map[K]V {
+func Merge[M ~map[K]V, K comparable, V any](maps ...M) map[K]V {
 	result := make(map[K]V)
 	for _, m := range maps {
 		for k, v := range m {
 			result[k] = v
 		}
-	}
-	return result
-}
-
-// 获取 map 的 key 列表
-func Keys[K comparable, V any](m map[K]V) []K {
-	result := make([]K, len(m))
-	for k := range m {
-		result = append(result, k)
-	}
-	return result
-}
-
-// 获取 map 的 value 列表
-func Values[K comparable, V any](m map[K]V) []V {
-	result := make([]V, len(m))
-	for _, v := range m {
-		result = append(result, v)
 	}
 	return result
 }
@@ -63,4 +45,92 @@ func Remap[K comparable, V any, U any](m map[K]V, f func(k K, v V) U) map[K]U {
 		result[k] = f(k, v)
 	}
 	return result
+}
+
+// Keys returns the keys of the map m.
+// The keys will be in an indeterminate order.
+func Keys[M ~map[K]V, K comparable, V any](m M) []K {
+	r := make([]K, 0, len(m))
+	for k := range m {
+		r = append(r, k)
+	}
+	return r
+}
+
+// Values returns the values of the map m.
+// The values will be in an indeterminate order.
+func Values[M ~map[K]V, K comparable, V any](m M) []V {
+	r := make([]V, 0, len(m))
+	for _, v := range m {
+		r = append(r, v)
+	}
+	return r
+}
+
+// Equal reports whether two maps contain the same key/value pairs.
+// Values are compared using ==.
+func Equal[M1, M2 ~map[K]V, K, V comparable](m1 M1, m2 M2) bool {
+	if len(m1) != len(m2) {
+		return false
+	}
+	for k, v1 := range m1 {
+		if v2, ok := m2[k]; !ok || v1 != v2 {
+			return false
+		}
+	}
+	return true
+}
+
+// EqualFunc is like Equal, but compares values using eq.
+// Keys are still compared with ==.
+func EqualFunc[M1 ~map[K]V1, M2 ~map[K]V2, K comparable, V1, V2 any](m1 M1, m2 M2, eq func(V1, V2) bool) bool {
+	if len(m1) != len(m2) {
+		return false
+	}
+	for k, v1 := range m1 {
+		if v2, ok := m2[k]; !ok || !eq(v1, v2) {
+			return false
+		}
+	}
+	return true
+}
+
+// Clear removes all entries from m, leaving it empty.
+func Clear[M ~map[K]V, K comparable, V any](m M) {
+	for k := range m {
+		delete(m, k)
+	}
+}
+
+// Clone returns a copy of m.  This is a shallow clone:
+// the new keys and values are set using ordinary assignment.
+func Clone[M ~map[K]V, K comparable, V any](m M) M {
+	// Preserve nil in case it matters.
+	if m == nil {
+		return nil
+	}
+	r := make(M, len(m))
+	for k, v := range m {
+		r[k] = v
+	}
+	return r
+}
+
+// Copy copies all key/value pairs in src adding them to dst.
+// When a key in src is already present in dst,
+// the value in dst will be overwritten by the value associated
+// with the key in src.
+func Copy[M1 ~map[K]V, M2 ~map[K]V, K comparable, V any](dst M1, src M2) {
+	for k, v := range src {
+		dst[k] = v
+	}
+}
+
+// DeleteFunc deletes any key/value pairs from m for which del returns true.
+func DeleteFunc[M ~map[K]V, K comparable, V any](m M, del func(K, V) bool) {
+	for k, v := range m {
+		if del(k, v) {
+			delete(m, k)
+		}
+	}
 }
